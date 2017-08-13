e5d9ad63d0f3463903fbc616bfb729f6d8aedc43
Konstantinos Dimakis <dimakisblogging@gmail.com>
2017-08-13 17:17:38 +0300

    chore: Generate some needed templates / views

---
+++ app/scripts/views/food-view.js
@@ -0,0 +1,30 @@
+/*global HealthTracker, Backbone, JST*/
+
+HealthTracker.Views = HealthTracker.Views || {};
+
+(function () {
+  'use strict';
+
+  HealthTracker.Views.FoodView = Backbone.View.extend({
+
+    template: JST['app/scripts/templates/food-view.ejs'],
+
+    tagName: 'div',
+
+    id: '',
+
+    className: '',
+
+    events: {},
+
+    initialize: function () {
+      this.listenTo(this.model, 'change', this.render);
+    },
+
+    render: function () {
+      this.$el.html(this.template(this.model.toJSON()));
+    }
+
+  });
+
+})();
---
+++ app/scripts/views/stats-view.js
@@ -0,0 +1,30 @@
+/*global HealthTracker, Backbone, JST*/
+
+HealthTracker.Views = HealthTracker.Views || {};
+
+(function () {
+  'use strict';
+
+  HealthTracker.Views.StatsView = Backbone.View.extend({
+
+    template: JST['app/scripts/templates/stats-view.ejs'],
+
+    tagName: 'div',
+
+    id: '',
+
+    className: '',
+
+    events: {},
+
+    initialize: function () {
+      this.listenTo(this.model, 'change', this.render);
+    },
+
+    render: function () {
+      this.$el.html(this.template(this.model.toJSON()));
+    }
+
+  });
+
+})();
