de111241bf80578d822bbe3930e9f47f7babd4d0
Konstantinos Dimakis <dimakisblogging@gmail.com>
2017-08-15 01:23:17 +0300

    refactor: Package the API calls to NUTRITIONIX_API

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -9,58 +9,73 @@ var HealthTracker = {
   init: function () {
     'use strict';
     console.log('Hello from Backbone!');
-    // TODO: Make a call to nutrients API
-    // TODO: Package the calls in a MUCH PRETTIER way. Be sure to checkout how it can be done from inside Backbone
-    var instantHeaders = new Headers();
-    var nutritientHeaders = new Headers();
-    instantHeaders.append('x-app-id', '84d67ad4');
-    instantHeaders.append('x-app-key', '3e892392c1929c39d4afdd9a44e14d60');
-
-    nutritientHeaders.append('x-app-id', '84d67ad4');
-    nutritientHeaders.append('x-app-key', '3e892392c1929c39d4afdd9a44e14d60');
-    nutritientHeaders.append('x-remote-user-id', '0');
-    nutritientHeaders.append('Content-Type', 'application/json');
-
-    var instantOptions = {
-      headers: instantHeaders
-    };
-    var nutritientOptions = {
-      method: 'POST',
-      headers: nutritientHeaders,
-      body: JSON.stringify({
-          'query': 'banana'
-        })
-    };
-    var instantRequest = 'https://trackapi.nutritionix.com/v2/search/instant?' +
-      'query=banana&' +
-      'self=false';
-    var nutrientRequest = 'https://trackapi.nutritionix.com/v2/natural/nutrients';
-    fetch(instantRequest, instantOptions).then(function(response) {
-      if (!response.ok) {
-        let msg = 'There was a problem. ' + response.status + ': ' +
-          response.statusText;
-        console.error(msg);
-      } else {
-        response.json().then(function(data) {
-          console.log(data);
+    // TODO: Now parse through the response and return a promise with the parsed response as argument
+    const NUTRITIONIX_API = {
+      NUTRITIONIX_X_APP_ID: '84d67ad4',
+      NUTRITIONIX_X_APP_KEY: '3e892392c1929c39d4afdd9a44e14d60',
+      getResults: function(query) {
+        // check the argument
+        if (!query) {
+          console.error(Error('No query argument was found in getResults'));
+          return;
+        }
+        // build the request
+        const request = 'https://trackapi.nutritionix.com/v2/search/instant?self=false&query=' + query;
+        // add custom headers make the request
+        fetch(request, {
+          headers: {
+            'x-app-id': this.NUTRITIONIX_X_APP_ID,
+            'x-app-key': this.NUTRITIONIX_X_APP_KEY
+          }
+        }).then(function(response) {
+          if (!response.ok) {
+            let msg = 'There was a problem. ' + response.status + ': ' +
+              response.statusText;
+            console.error(msg);
+          } else {
+            response.json().then(function(data) {
+              console.log(data);
+            });
+          }
+        }).catch(function(error) {
+          console.error('There has been a problem with your fetch operation: ' + error.message);
+        });
+      },
+      getNutrients: function(query) {
+        // check the argument
+        if (!query) {
+          console.error(Error('No query argument was found in getResults'));
+          return;
+        }
+        // build the request
+        const request = 'https://trackapi.nutritionix.com/v2/natural/nutrients';
+        // add custom headers make the request
+        fetch(request, {
+          method: 'POST',
+          headers: {
+            'x-app-id': this.NUTRITIONIX_X_APP_ID,
+            'x-app-key': this.NUTRITIONIX_X_APP_KEY,
+            'x-remote-user-id': '0',
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify({
+            'query': query
+          })
+        }).then(function(response) {
+          if (!response.ok) {
+            let msg = 'There was a problem. ' + response.status + ': ' +
+              response.statusText;
+            console.error(msg);
+          } else {
+            response.json().then(function(data) {
+              console.log(data);
+            });
+          }
+        }).catch(function(error) {
+          console.error('There has been a problem with your fetch operation: ' + error.message);
         });
       }
-    }).catch(function(error) {
-      console.error('There has been a problem with your fetch operation: ' + error.message);
-    });
-    fetch(nutrientRequest, nutritientOptions).then(function(response) {
-      if (!response.ok) {
-        let msg = 'There was a problem. ' + response.status + ': ' +
-          response.statusText;
-        console.error(msg);
-      } else {
-        response.json().then(function(data) {
-          console.log(data);
-        });
-      }
-    }).catch(function(error) {
-      console.error('There has been a problem with your fetch operation: ' + error.message);
-    });
+    };
   }
 };
 
