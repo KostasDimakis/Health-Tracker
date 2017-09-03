365fd5fe33dd80e0e42f9b1d33d7bbe290a3b4d2
Konstantinos Dimakis <dimakisblogging@gmail.com>
2017-09-03 21:03:35 +0300

    chore: Introduce daily view

---
+++ app/scripts/views/daily-view.js
@@ -0,0 +1,30 @@
+/*global HealthTracker, Backbone, JST*/
+
+HealthTracker.Views = HealthTracker.Views || {};
+
+(function () {
+  'use strict';
+
+  HealthTracker.Views.DailyView = Backbone.View.extend({
+
+    template: JST['app/scripts/templates/daily-view.ejs'],
+
+    tagName: 'div',
+
+    id: '',
+
+    className: '',
+
+    events: {},
+
+    initialize: function () {
+      this.listenTo(this.model, 'change', this.render);
+    },
+
+    render: function () {
+      this.$el.html(this.template(this.model.toJSON()));
+    }
+
+  });
+
+})();
