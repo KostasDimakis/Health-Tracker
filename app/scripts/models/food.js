b5b024907ce09c4494562a5256400b7b5b11984a
KonstantinosDimakis <dimakisblogging@gmail.com>
2017-09-10 23:54:49 +0300

    refactor: Date attribute, now shows date & time

--- app/scripts/models/food.js
+++ app/scripts/models/food.js
@@ -12,19 +12,21 @@ HealthTracker.Models = HealthTracker.Models || {};
 
     defaults: {
       timestamp: Date.now(),
-      date: (new Date(Date.now()).toDateString()),
       // In case no image was provided
       img: 'https://www.nutritionix.com/images/apple-touch-icon-152x152.png',
       fats: 0,
       proteins: 0,
       carbohydrates: 0,
       calories: 0
+      // date attribute, see parse method
     },
 
     validate: function(attrs, options) {
     },
 
     parse: function(response, options)  {
+      // Add a local time & date attribute to the model
+      response.date = new Date(response.timestamp).toLocaleString();
       return response;
     }
   });
